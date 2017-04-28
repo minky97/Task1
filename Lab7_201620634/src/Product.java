2449526b2b5fc2ee3078516ae86e66eefc8e6f67
leemk <leemk@DESKTOP-A2KGL19>
2017-04-28 16:51:08 +0900

    product

--- Lab7_201620634/src/Product.java
+++ Lab7_201620634/src/Product.java
@@ -1,10 +1,20 @@
 
 public abstract class Product {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hi!");
-System.out.println("kkk");
-	}
+private double price;
 
+public double getPrice() {
+	return price;
+}
+
+public void setPrice(double price) {
+	
+	if(price<0){
+		System.out.println("Error !");
+		price=0;
+	}
+	this.price = price;
+	
+}
+public abstract String getName();
 }
