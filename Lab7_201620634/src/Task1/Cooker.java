cd1723d4d275e9d97a247e3d7ab4efe04d25839f
leemk <leemk@DESKTOP-A2KGL19>
2017-04-30 03:06:21 +0900

    ?

---
+++ Lab7_201620634/src/Task1/Cooker.java
@@ -0,0 +1,6 @@
+package Task1;
+
+public interface Cooker {
+
+	public abstract String prepareFood();
+}
---
+++ Lab7_201620634/src/Task2/our.java
@@ -0,0 +1,10 @@
+package Task2;
+
+public class our {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("mong mong !");
+	}
+
+}
