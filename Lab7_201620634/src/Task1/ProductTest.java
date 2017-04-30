d379285d3bc7943815fe499da92b032d29bc1fb3
진희 <@192.168.0.149>
2017-04-30 16:58:01 +0900

    .

--- Lab7_201620634/src/Task1/ProductTest.java
+++ Lab7_201620634/src/Task1/ProductTest.java
@@ -1,11 +1,18 @@
 package Task1;
 public class ProductTest {
 
+	SmartTV s =new SmartTV ();
+	Laptop l = new Laptop();
+	Toaster t = new Toaster();
+	
 	public ProductTest(){
 		
 	}
-	public void testProduct(Product p){
-		
+	public void testProduct(String a){
+		if(a.equals("SmartTV")) System.out.println("product name is"+s.getName());
+		else if(a.equals("Laptop")) System.out.println("product name is"+l.getName());
+		else if(a.equals("Toaster")) System.out.println("product name is"+t.getName());
+		else System.out.println("Error!!!!1");
 	}
 	public void testDataStorage(DataStorage l){
 		System.out.println("FreeCapacity");
